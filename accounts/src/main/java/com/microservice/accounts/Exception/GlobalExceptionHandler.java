45002ac58904e3f431efa79bbc3aa6d3da7731e9
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-05-21 18:30:20 +0530

    changes

--- accounts/src/main/java/com/microservice/accounts/Exception/GlobalExceptionHandler.java
+++ accounts/src/main/java/com/microservice/accounts/Exception/GlobalExceptionHandler.java
@@ -1,5 +1,25 @@
 package com.microservice.accounts.Exception;
 
+import com.microservice.accounts.dto.ErrorResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
 
+    public ResponseEntity<ErrorResponseDto> handleCustomerAlreadyExistsException(CustomerAlreadyExistsExceptions exceptions, WebRequest webRequest) {
+
+        ErrorResponseDto errorResponseDto = new ErrorResponseDto(
+                webRequest.getDescription(false),
+                HttpStatus.BAD_REQUEST,
+                exceptions.getMessage(),
+                LocalDateTime.now()
+        );
+
+        return new ResponseEntity<>(errorResponseDto, HttpStatus.BAD_REQUEST);
+    }
 }
