c94e7b736ad7fe128f8790ea0842fbd761b3c3af
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-05-21 15:17:04 +0530

    changes

---
+++ accounts/src/main/java/com/microservice/accounts/Constants/AccountConstants.java
@@ -0,0 +1,15 @@
+package com.microservice.accounts.Constants;
+
+public class AccountConstants {
+    private AccountConstants() {
+
+    }
+    public static final String SAVING = "SAVING";
+    public static final String ADDRESS = "123 MAIN STREET, NEW YORK";
+    public static final String STATUS_201 = "201";
+    public static final String MESSAGE_201 = "ACCOUNT CREATED SUCCESSFULLY";
+    public static final String STATUS_200 = "200";
+    public static final String MESSAGE_200 = "REQUEST PROCESS SUCCESSFULLY";
+    public static final String STATUS_500 = "500";
+    public static final String MESSAGE_500 = "An error occured while processing your request,Please try again or contact with Dev team";
+}
---
+++ accounts/src/main/java/com/microservice/accounts/Controller/AccountController.java
@@ -0,0 +1,22 @@
+package com.microservice.accounts.Controller;
+
+import com.microservice.accounts.dto.CustomerDto;
+import com.microservice.accounts.dto.ResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+@RequestMapping(path = "/api",produces = MediaType.APPLICATION_JSON_VALUE)
+public class AccountController {
+
+    public ResponseEntity<ResponseDto>createAccount(@RequestBody CustomerDto customerDto){
+     return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseDto());
+    }
+
+}
