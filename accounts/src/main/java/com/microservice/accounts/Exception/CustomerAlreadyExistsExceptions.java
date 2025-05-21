10f7e31279ab8e26f70e60f2ea447d94d7978134
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-05-21 17:28:26 +0530

    changes

---
+++ accounts/src/main/java/com/microservice/accounts/Exception/CustomerAlreadyExistsExceptions.java
@@ -0,0 +1,12 @@
+package com.microservice.accounts.Exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+public class CustomerAlreadyExistsExceptions extends RuntimeException {
+
+    public CustomerAlreadyExistsExceptions(String message) {
+        super(message);
+    }
+}
