276ece26596b5c5ac6d2c27d0c03fcb9cb172dea
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-05-21 14:36:53 +0530

    changes

---
+++ accounts/src/main/java/com/microservice/accounts/Repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.microservice.accounts.Repository;
+
+import com.microservice.accounts.Entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer,Long> {
+}
