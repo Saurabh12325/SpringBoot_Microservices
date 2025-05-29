35f9f114c57289a9759e177e9c9a8fdb92e14f44
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-05-29 18:40:35 +0530

    spring docs

--- accounts/src/main/java/com/microservice/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/microservice/accounts/AccountsApplication.java
@@ -1,5 +1,10 @@
 package com.microservice.accounts;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -7,6 +12,29 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Accounts Microservice REST API Documentation",
+				description = "Banking Microservices - Accounts Microservice",
+				version = "v3",
+				contact = @Contact(
+						name = "Saurabh Srivastav",
+						email = "H2X4F@example.com",
+						url = "https://github.com/saurabhsrivastav"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.apache.org/licenses/LICENSE-2.0"
+				)
+
+				),
+		externalDocs = @ExternalDocumentation(
+				description = "Accounts Microservice Documentation",
+				url = "https://github.com/saurabhsrivastav/microservices/tree/main/accounts"
+		)
+
+		)
+
 public class AccountsApplication {
 
 	public static void main(String[] args) {
